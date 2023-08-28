18fe6709e15b6d92d5ab3478a97731ab88a39f72
CHIPATISWATHIREDDY <113338947+CSwathi1518@users.noreply.github.com>
2023-08-28 12:08:55 +0530

    Update 4.follow.c

--- 4.follow.c
+++ 4.follow.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<ctype.h>
 #include<string.h>
 int n,m=0,p,i=0,j=0;
 char a[10][10],f[10];
@@ -6,31 +7,31 @@ void follow(char c);
 void first(char c);
 int main()
 {
- int i,choice;
+ int i,z;
  char c,ch;
  printf("Enter the no.of productions:");
  scanf("%d",&n);
- printf("Enter the productions(epsilon=@):\n");
+ printf("Enter the productions(epsilon=$):\n");
  for(i=0;i<n;i++)
   scanf("%s%c",a[i],&ch);
-do
+ do
  {
   m=0;
   printf("Enter the element whose FOLLOW is to be found:");
-scanf("%c",&c);
+  scanf("%c",&c);
   follow(c);
   printf("FOLLOW(%c) = { ",c);
   for(i=0;i<m;i++)
    printf("%c ",f[i]);
   printf(" }\n");
   printf("Do you want to continue(0/1)?");
-  scanf("%d%c",&choice,&ch);
+  scanf("%d%c",&z,&ch);
  }
- while(choice==1);
+ while(z==1);
 }
 void follow(char c)
 {
- if(a[0][0]==c)f[m++]='e';
+ if(a[0][0]==c)f[m++]='$';
  for(i=0;i<n;i++)
  {
   for(j=2;j<strlen(a[i]);j++)
@@ -39,7 +40,7 @@ void follow(char c)
    {
     if(a[i][j+1]!='\0')first(a[i][j+1]);
     if(a[i][j+1]=='\0'&&c!=a[i][0])
-    follow(a[i][0]);
+     follow(a[i][0]);
    }
   }
  }
@@ -49,15 +50,12 @@ void first(char c)
     int k;
     if(!(isupper(c)))f[m++]=c;
     for(k=0;k<n;k++)
-{
+    {
     if(a[k][0]==c)
-     {
-        if(a[k][2]=='@')
-  		follow(a[i][0]);
-        else if(islower(a[k][2]))
-		f[m++]=a[k][2];
-        else 
-		first(a[k][2]);
+    {
+    if(a[k][2]=='$') follow(a[i][0]);
+    else if(islower(a[k][2]))f[m++]=a[k][2];
+    else first(a[k][2]);
     }
-}
+    }
 }
